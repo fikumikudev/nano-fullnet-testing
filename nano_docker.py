aead4b026933cc467239c5f7a7b93935c6d527a0
fikumikudev <3044353+fikumikudev@users.noreply.github.com>
2022-06-06 15:11:35 +0200

    up

--- nano_docker.py
+++ nano_docker.py
@@ -1,6 +1,7 @@
 import os
 
 import docker
+import dotenv
 import nano
 from retry import retry
 
@@ -9,31 +10,68 @@ import common
 RPC_PORT = 17076
 
 
+class NanoWalletAccount:
+    def __init__(self, wallet, account_id):
+        self.node = wallet.node
+        self.wallet = wallet
+        self.account_id = account_id
+
+    def print_info(self):
+        print()
+        print("account:", self.account_id)
+        print("balance:", self.balance)
+
+    @property
+    def balance(self):
+        res = self.node.rpc.account_balance(self.account_id)
+        return (res["balance"], res["pending"])
+
+
+class NanoWallet:
+    def __init__(self, node, wallet_id):
+        self.node = node
+        self.wallet_id = wallet_id
+
+    def create_account(self, private_key=None):
+        if not private_key:
+            account_id = self.node.rpc.account_create(wallet=self.wallet_id)
+        else:
+            account_id = self.node.rpc.wallet_add(wallet=self.wallet_id, key=private_key)
+        return NanoWalletAccount(self, account_id)
+
+
 class NanoNode:
     def __init__(self, container):
         self.container = container
         self.rpc = nano.rpc.Client(f"http://localhost:{self.host_rpc_port}")
-        pass
 
     @retry(tries=5, delay=1)
     def ensure_started(self):
         self.rpc.version()
 
     def print_info(self):
+        print()
         print("name:", self.container.name)
         print("port:", self.host_rpc_port)
         print("block count:", self.rpc.block_count())
         print("version:", self.rpc.version())
-        pass
 
     @property
     def host_rpc_port(self):
         return int(self.container.ports[f"{RPC_PORT}/tcp"][0]["HostPort"])
 
-    pass
+    @property
+    def name(self):
+        return self.container.name
+
+    def create_wallet(self, private_key=None):
+        wallet_id = self.rpc.wallet_create()
+        wallet = NanoWallet(self, wallet_id)
+        account = wallet.create_account(private_key=private_key)
+        return wallet, account
 
 
-class NanoNet:
+class NanoTest:
     name_prefix = "nano-baseline"
 
     __node_containers = []
@@ -42,21 +80,43 @@ class NanoNet:
         self.network_name = f"{self.name_prefix}_network"
 
         self.client = docker.from_env()
-        self.node_env = common.load_env_data_as_dict("node.env")
+        self.node_env = dotenv.dotenv_values("node.env")
 
         self.__cleanup_docker()
 
         self.__setup_network()
+        self.__setup_genesis()
 
-        self.main_node = self.__create_node()
-
-        node_1 = self.__create_node(default_peer=self.main_node)
+        node_1 = self.__create_node(default_peer=self.genesis_node)
 
         pass
 
+    def __setup_genesis(self):
+        node, wallet, account = self.create_node_with_account(
+            private_key=self.node_env["NANO_TEST_GENESIS_PRIV"]
+        )
+        self.genesis_node = node
+        self.genesis_wallet = wallet
+        self.genesis_account = account
+
+    def __setup_network(self):
+        if self.client.networks.list(names=[self.network_name]):
+            self.network = self.client.networks.get(self.network_name)
+        else:
+            self.network = self.client.networks.create(
+                self.network_name, check_duplicate=True
+            )
+
+    def create_node(self):
+        return self.__create_node()
+
+    def create_node_with_account(self, private_key=None):
+        node = self.create_node()
+        wallet, account = node.create_wallet(private_key=private_key)
+        return node, wallet, account
+
     def __create_node(self, image_name="nano-node", default_peer=None):
         node_cli_options = "--network=test --data_path /root/Nano/"
-        # node_cli_options = "--network=test"
         node_command = f"nano_node daemon ${node_cli_options} -l"
         node_main_command = (
             f"nano_node daemon {node_cli_options} --config node.peering_port=17075 -l"
@@ -70,7 +130,11 @@ class NanoNet:
                 **self.node_env,
             }
         else:
-            env = self.node_env
+            env = {
+                "NANO_DEFAULT_PEER": "0",
+                "NANO_TEST_PEER_NETWORK": "0",
+                **self.node_env,
+            }
 
         container = self.client.containers.run(
             image_name,
@@ -101,26 +165,18 @@ class NanoNet:
 
         return node
 
-    def __setup_network(self):
-        if self.client.networks.list(names=[self.network_name]):
-            self.network = self.client.networks.get(self.network_name)
-        else:
-            self.network = self.client.networks.create(
-                self.network_name, check_duplicate=True
-            )
-
     def __cleanup_docker(self):
         for cont in self.client.containers.list():
             if cont.name.startswith(self.name_prefix):
                 cont.remove(force=True)
 
 
-def main():
-    nano_test = NanoNet()
+def create():
+    nano_test = NanoTest()
     nano_test.setup()
 
-    pass
+    return nano_test
 
 
 if __name__ == "__main__":
-    main()
+    create()
