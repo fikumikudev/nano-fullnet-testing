a26b0786f8937ac8d22383535513e99ae6ed95ed
fikumikudev <3044353+fikumikudev@users.noreply.github.com>
2022-06-07 15:33:18 +0200

    bin tree spam test

--- nano_docker.py
+++ nano_docker.py
@@ -1,4 +1,5 @@
 import os
+from typing import Union, Tuple
 
 import docker
 import dotenv
@@ -10,11 +11,20 @@ import common
 
 RPC_PORT = 17076
 BURN_ACCOUNT = "nano_1111111111111111111111111111111111111111111111111111hifc8npp"
-DEFAULT_REPR = "nano_000000000000000000000000000000000000000000000000000000000000"
+DEFAULT_REPR = BURN_ACCOUNT
+DIFFICULTY = "0000000000000000"
+
+
+def account_id_from_account(account):
+    if hasattr(account, "account_id"):
+        account_id = account.account_id
+    else:
+        account_id = account
+    return account_id
 
 
 class Block:
-    def __init__(self, block_nlib: nanolib.Block, prev_block):
+    def __init__(self, block_nlib: nanolib.Block, prev_block: "Block"):
         self.block_nlib = block_nlib
         self.prev_block = prev_block
 
@@ -41,6 +51,9 @@ class Block:
             raise ValueError("Not a send block")
         return diff
 
+    def json(self):
+        return self.block_nlib.json()
+
 
 class Chain:
     __unpublished = []
@@ -57,28 +70,36 @@ class Chain:
         private_key = nanolib.generate_account_private_key(seed, 0)
         return Chain(account_id, private_key, None)
 
-    def send(self, account, amount):
+    @property
+    def balance(self):
+        return self.frontier.balance
+
+    def send(self, account: Union["NanoWalletAccount", "Chain", str], amount):
         if amount <= 0:
             raise ValueError("Amount must be positive")
         if not self.frontier:
             raise ValueError("Account not opened")
 
-        account_id = account.account_id
-        block_nlib = Block(
+        destination_id = account_id_from_account(account)
+
+        block_nlib = nanolib.Block(
             block_type="state",
             account=self.account_id,
             representative=self.frontier.representative,
             previous=self.frontier.block_hash,
-            link_as_account=account_id,
+            link_as_account=destination_id,
             balance=self.frontier.balance - amount,
         )
+        block_nlib.sign(self.private_key)
+        block_nlib.solve_work(DIFFICULTY)
 
         block = Block(block_nlib, self.frontier)
+
         self.frontier = block
         self.__unpublished.append(block)
         return block
 
-    def receive(self, block: Block, representative=None):
+    def receive(self, block: Block, representative=None) -> Block:
         if not self.frontier:
             # open account
 
@@ -87,14 +108,17 @@ class Chain:
             else:
                 representative_id = representative.account_id
 
-            block_nlib = Block(
+            block_nlib = nanolib.Block(
                 block_type="state",
                 account=self.account_id,
                 representative=representative_id,
-                previous=self.frontier.block_hash,
-                link_as_account=self.account_id,
+                previous=None,
+                link=block.block_hash,
                 balance=block.send_amount,
             )
+            block_nlib.sign(self.private_key)
+            block_nlib.solve_work(DIFFICULTY)
+
             block = Block(block_nlib, None)
 
         else:
@@ -103,19 +127,28 @@ class Chain:
             else:
                 representative = representative.account_id
 
-            block_nlib = Block(
+            block_nlib = nanolib.Block(
                 block_type="state",
                 account=self.account_id,
                 representative=representative_id,
                 previous=self.frontier.block_hash,
-                link_as_account=self.account_id,
+                link=block.block_hash,
                 balance=int(self.frontier.balance + block.send_amount),
             )
+            block_nlib.sign(self.private_key)
+            block_nlib.solve_work(DIFFICULTY)
+
             block = Block(block_nlib, self.frontier)
 
         self.frontier = block
         self.__unpublished.append(block)
         return block
+
+    def publish(self, node: "NanoNode"):
+        cnt = len(self.__unpublished)
+        hashes = [node.publish(block) for block in self.__unpublished]
+        self.__unpublished.clear()
+        return cnt, hashes
 
 
 class NanoWalletAccount:
@@ -141,24 +174,22 @@ class NanoWalletAccount:
         res = self.node.rpc.account_balance(self.account_id)
         return res["pending"]
 
-    def send(self, account, amount):
-        if isinstance(account, NanoWalletAccount):
-            destination_id = account.account_id
-        else:
-            destination_id = account
+    def send(self, account: Union["NanoWalletAccount", Chain, str], amount) -> Block:
+        destination_id = account_id_from_account(account)
 
-        res = self.node.rpc.send(
+        block_hash = self.node.rpc.send(
             wallet=self.wallet.wallet_id,
             source=self.account_id,
             destination=destination_id,
             amount=amount,
         )
 
-    def to_chain(self):
+        block = self.node.block(block_hash)
+        return block
+
+    def to_chain(self) -> Chain:
         frontier_hash = self.node.rpc.account_info(self.account_id)["frontier"]
-        frontier_dict = self.node.rpc.block(frontier_hash)
-        frontier_nlib = nanolib.Block.from_dict(frontier_dict, verify=False)
-        frontier = Block(frontier_nlib, None)
+        frontier = self.node.block(frontier_hash)
         return Chain(self.account_id, self.private_key, frontier)
 
 
@@ -167,7 +198,7 @@ class NanoWallet:
         self.node = node
         self.wallet_id = wallet_id
 
-    def create_account(self, private_key=None):
+    def create_account(self, private_key=None) -> NanoWalletAccount:
         if not private_key:
             seed = nanolib.generate_seed()
             private_key = nanolib.generate_account_private_key(seed, 0)
@@ -209,12 +240,32 @@ class NanoNode:
         account = wallet.create_account(private_key=private_key)
         return wallet, account
 
+    def publish(self, block: Block):
+        return self.rpc.process(block.json())
+
+    def block(self, hash: str, load_previous=True) -> Block:
+        block_nlib = self.__nlib_block(hash)
+
+        if load_previous and block_nlib.previous:
+            prev_block = self.block(block_nlib.previous, load_previous=False)
+            block = Block(block_nlib, prev_block)
+        else:
+            block = Block(block_nlib, None)
+
+        return block
+
+    def __nlib_block(self, hash):
+        block_dict = self.rpc.block(hash)
+        block_nlib = nanolib.Block.from_dict(block_dict, verify=False)
+        return block_nlib
+
 
 class NanoTest:
     name_prefix = "nano-baseline"
 
-    nodes = []
-    __node_containers = []
+    def __init__(self):
+        self.nodes = []
+        self.__node_containers = []
 
     def setup(self):
         self.network_name = f"{self.name_prefix}_network"
@@ -253,7 +304,7 @@ class NanoTest:
             if cont.name.startswith(self.name_prefix):
                 cont.remove(force=True)
 
-    def create_node(self):
+    def create_node(self) -> NanoNode:
         node = self.__create_node_container()
         node.ensure_started()
         node.print_info()
@@ -261,7 +312,9 @@ class NanoTest:
         self.nodes.append(node)
         return node
 
-    def create_node_with_account(self, private_key=None):
+    def create_node_with_account(
+        self, private_key=None
+    ) -> Tuple[NanoNode, NanoWallet, NanoWalletAccount]:
         node = self.create_node()
         wallet, account = node.create_wallet(private_key=private_key)
         return node, wallet, account
@@ -318,6 +371,7 @@ class NanoTest:
         return self.genesis_node, self.genesis_account
 
     def ensure_all_confirmed(self):
+        print(">> ensure_all_confirmed:")
         for node in self.nodes:
             node.print_info()
 
--- tests.py
+++ tests.py
@@ -1,23 +1,39 @@
+from itertools import chain
 import unittest
+from cmath import nan
+from collections import deque
 
 import nano_docker as nanotest
 
+
+def spam_bin_tree(node, amount, source_account, count):
+    chain_root = nanotest.Chain.random_account()
+    chain_root.receive(source_account.send(chain_root, amount))
+    chain_root.publish(node)
+
+    q = deque([chain_root])
+
+    for i in range(count):
+        r = q.popleft()
+        a = nanotest.Chain.random_account()
+        b = nanotest.Chain.random_account()
+
+        half_balance = int(r.balance / 2)
+        a.receive(r.send(a, half_balance))
+        b.receive(r.send(b, half_balance))
+
+        r.publish(node)
+        a.publish(node)
+        b.publish(node)
+
+        q.append(a)
+        q.append(b)
+
+        if i % 100 == 0:
+            print("spam bin_tree:", i)
+
+
 class TestStringMethods(unittest.TestCase):
-
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
-
     def test_nano(self):
         nano_test = nanotest.create()
 
@@ -27,18 +43,27 @@ class TestStringMethods(unittest.TestCase):
         nano_test.genesis_account.print_info()
 
         # uniformly distribute rep voting weight
-        reserved = 2 ^ 20
+        reserved = 2 ** 20
         balance_left = nano_test.genesis_account.balance - reserved
         balance_per_rep = int(balance_left / len(reps))
         for _, _, rep_account in reps:
-            nano_test.genesis_account.send(rep_account, balance_per_rep) 
+            nano_test.genesis_account.send(rep_account, balance_per_rep)
 
         nano_test.ensure_all_confirmed()
 
         genesis_chain = nano_test.genesis_account.to_chain()
 
+        spam_bin_tree(
+            nano_test.genesis_node,
+            amount=reserved,
+            source_account=nano_test.genesis_account,
+            count=500,
+        )
+
+        nano_test.ensure_all_confirmed()
+
         pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
